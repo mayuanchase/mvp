90ae6976960dc2c84fc4b4a5a7197c3bb6a9b7f8
mayuan <mayuan@qunhemail.com>
2019-11-20 14:24:25 +0800

    测试大纲页面

---
+++ src/components/outline/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import './index.css'
+
+export default class Outline extends React.Component {
+
+    public render() {
+        return (
+            <div className="outline">
+                <span>OKR 大纲</span>
+            </div>
+
+        );
+    }
+}
